52c9601a3b222a975124427f488f30210903f619
SaeRyung <sr1094@naver.com>
2024-09-19 18:01:19 +0900

    feat/comu/create: #22 커뮤니티 게시판 글 등록 기능 추가

---
+++ back-end/with-the-developer/src/main/java/com/developer/comu/command/dto/ComuPostDTO.java
@@ -0,0 +1,22 @@
+package com.developer.comu.command.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.apache.catalina.User;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class ComuPostDTO {
+
+    private int comuCode;
+    private String comuSubject;
+    private String comuContent;
+    private LocalDateTime comuCreateDate;
+    private LocalDateTime comuUpdateDate;
+    private boolean comuDelStatus;
+    private User userCode;
+}
---
+++ back-end/with-the-developer/src/main/java/com/developer/comu/command/repository/ComuPostRepository.java
@@ -0,0 +1,7 @@
+package com.developer.comu.command.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ComuPostRepository extends JpaRepository {
+}
