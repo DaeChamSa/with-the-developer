b9bdc5d7b6f026447ff89d48b185e4a08fe9db1a
Younggi Kim <galaxy138742@gmail.com>
2024-09-22 21:09:52 +0900

    refactor: 유효성 검사 추가

--- back-end/with-the-developer/src/main/java/com/developer/project/command/application/dto/ProjPostRequestDTO.java
+++ back-end/with-the-developer/src/main/java/com/developer/project/command/application/dto/ProjPostRequestDTO.java
@@ -1,13 +1,20 @@
 package com.developer.project.command.application.dto;
 
 import com.developer.project.command.domain.aggregate.ProjPost;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
 public class ProjPostRequestDTO {
 
+    @NotNull(message = "제목은 필수입니다.")
+    @NotBlank(message = "제목은 공백일 수 없습니다.")
     private String projPostTitle;
+
+    @NotNull(message = "내용은 필수입니다.")
     private String projPostContent;
+
     private String projUrl;
 
     public ProjPost toEntity() {
--- back-end/with-the-developer/src/main/java/com/developer/project/command/domain/aggregate/ProjPost.java
+++ back-end/with-the-developer/src/main/java/com/developer/project/command/domain/aggregate/ProjPost.java
@@ -1,10 +1,7 @@
 package com.developer.project.command.domain.aggregate;
 
 import com.developer.common.util.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,8 +18,10 @@ public class ProjPost extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long projPostCode;
 
+    @Column(nullable = false)
     private String projPostTitle;
 
+    @Column(nullable = false)
     private String projPostContent;
 
     private String projUrl;
