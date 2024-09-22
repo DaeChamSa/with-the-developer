c44ff768cc33949cca23571da41b770b0967ad93
Younggi Kim <galaxy138742@gmail.com>
2024-09-22 19:48:38 +0900

    feat: 에러 코드 추가

--- back-end/with-the-developer/src/main/java/com/developer/common/exception/ErrorCode.java
+++ back-end/with-the-developer/src/main/java/com/developer/common/exception/ErrorCode.java
@@ -19,12 +19,13 @@ public enum ErrorCode {
     NOT_MATCH_ROLE(HttpStatus.BAD_REQUEST, "잘못된 권한입니다."),
     NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
     NEED_LOGIN(HttpStatus.BAD_REQUEST, "로그인이 필요한 서비스입니다."),
-
+    NOT_MATCH_USERCODE(HttpStatus.BAD_REQUEST, "자신의 게시물이 아닙니다."),
 
     // == 404 ==
     NOT_FOUNDED_USER(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
     NOT_FOUNDED_ADMIN(HttpStatus.NOT_FOUND, "없는 관리자 계정입니다."),
     NOT_FOUNDED_CODE(HttpStatus.NOT_FOUND, "이메일 인증코드를 찾을 수 없습니다."),
+    NOT_FOUND_PROJ_POST(HttpStatus.NOT_FOUND, "게시물을 찾을 수 없습니다."),
 
     // == 500 ==
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류입니다.");
