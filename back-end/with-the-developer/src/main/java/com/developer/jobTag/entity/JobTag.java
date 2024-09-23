65084d339a92d0594b36a75568186ce1998c0574
hyomee2 <s5763305@naver.com>
2024-09-23 19:17:32 +0900

    chore: #44 오타 수정

--- back-end/with-the-developer/src/main/java/com/developer/jobTag/entity/JobTag.java
+++ back-end/with-the-developer/src/main/java/com/developer/jobTag/entity/JobTag.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "jot_tag")
+@Table(name = "job_tag")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class JobTag {
