f4067574a801fd506eaeafd709f51f858eb2ddd0
Younggi Kim <galaxy138742@gmail.com>
2024-09-24 16:22:27 +0900

    refactor: 의존성 주입 변경

--- back-end/with-the-developer/src/main/java/com/developer/project/post/query/service/ProjPostQueryService.java
+++ back-end/with-the-developer/src/main/java/com/developer/project/post/query/service/ProjPostQueryService.java
@@ -7,7 +7,6 @@ import com.developer.project.post.query.dto.ProjPostResponseDTO;
 import com.developer.project.post.query.mapper.ProjPostMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,14 +17,13 @@ import java.util.List;
 @Service
 public class ProjPostQueryService {
 
-    private final SqlSession sqlSession;
+    private final ProjPostMapper projPostMapper;
 
     @Transactional(readOnly = true)
     public List<ProjPostListResponseDTO> readAll(Integer page) {
-
         int offset = (page - 1) * 10;
 
-        List<ProjPostListResponseDTO> projPostList = sqlSession.getMapper(ProjPostMapper.class).findAll(offset);
+        List<ProjPostListResponseDTO> projPostList = projPostMapper.findAll(offset);
 
         if (projPostList == null) {
             throw new CustomException(ErrorCode.NOT_FOUND_POST);
@@ -36,7 +34,7 @@ public class ProjPostQueryService {
 
     @Transactional(readOnly = true)
     public ProjPostResponseDTO readByCode(Long projPostCode) {
-        ProjPostResponseDTO projPostResponseDTO = sqlSession.getMapper(ProjPostMapper.class).findByCode(projPostCode);
+        ProjPostResponseDTO projPostResponseDTO = projPostMapper.findByCode(projPostCode);
 
         if (projPostResponseDTO == null) {
             throw new CustomException(ErrorCode.NOT_FOUND_POST);
