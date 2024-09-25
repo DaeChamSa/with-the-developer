4ae925d477933821ab3e9e645de0155823082bbb
hyomee2 <s5763305@naver.com>
2024-09-25 14:46:36 +0900

    feat: #17 NotNull 엔티티 레벨에서 유효성 검사 추가

--- back-end/with-the-developer/src/main/java/com/developer/jobTag/entity/JobTag.java
+++ back-end/with-the-developer/src/main/java/com/developer/jobTag/entity/JobTag.java
@@ -1,6 +1,7 @@
 package com.developer.jobTag.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -16,6 +17,7 @@ public class JobTag {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long jobTagCode; // 직무태그 코드
 
+    @NotNull
     private String jobTagName;  // 직무 태그 이름
 
      public JobTag(String jobTagName) {
