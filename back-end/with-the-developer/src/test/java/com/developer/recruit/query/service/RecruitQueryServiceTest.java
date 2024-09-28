970c07d1662114fe4d16323e94c44f44f4721e60
hyomee2 <s5763305@naver.com>
2024-09-29 04:33:46 +0900

    test: #89 채용공고 목록 조회 테스트 코드 작성

--- back-end/with-the-developer/src/test/java/com/developer/recruit/query/service/RecruitQueryServiceTest.java
+++ back-end/with-the-developer/src/test/java/com/developer/recruit/query/service/RecruitQueryServiceTest.java
@@ -1,5 +1,6 @@
 package com.developer.recruit.query.service;
 
+import com.developer.recruit.command.entity.RecruitStatus;
 import com.developer.recruit.query.dto.RecruitDetailReadDTO;
 import com.developer.recruit.query.mapper.RecruitMapper;
 import org.junit.jupiter.api.DisplayName;
@@ -20,10 +21,30 @@ class RecruitQueryServiceTest {
     @Autowired
     private RecruitMapper recruitMapper;
 
+    @Autowired
+    private RecruitQueryService recruitQueryService;
+
+    @Test
+    @DisplayName("등록된 채용공고의 목록을 조회한다.")
+    void readRecruitList() {
+        // Given
+        // When
+        List<com.developer.recruit.query.dto.RecruitListReadDTO> recruitList = recruitQueryService.readRecruitList(1);
+        // Then
+        assertNotNull(recruitList);
+        assertFalse(recruitList.isEmpty());
+
+        assertEquals("기획 및 프론트엔드 개발자 모집", recruitList.get(0).getRecruitTitle());
+        assertEquals(LocalDateTime.parse("2024-10-01T09:00:00"), recruitList.get(0).getRecruitStart());
+        assertEquals(LocalDateTime.parse("2024-10-31T18:00:00"), recruitList.get(0).getRecruitEnd());
+        assertEquals(RecruitStatus.DELETE, recruitList.get(0).getRecruitStatus());
+        assertEquals("user02", recruitList.get(0).getUserId());
+        assertEquals(List.of("프론트엔드", "기획"), recruitList.get(0).getJobTagNames());
+    }
+
     @Test
     @DisplayName("채용공고 상세 내용을 조회한다.")
     void readRecruitDetailById() {
-
         // WHEN
         RecruitDetailReadDTO recruitDetailReadDTO = recruitMapper.readRecruitDetailById(1L);
 
