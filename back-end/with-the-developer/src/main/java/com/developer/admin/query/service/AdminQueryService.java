6414d5b3c7906d9bc8b9da0396e8a19e7a95bf5b
PBEM22 <dlacofbs1229@gmail.com>
2024-09-28 15:26:23 +0900

    chore: #85 sqlSession부분 Mapper로 전체 변경

--- back-end/with-the-developer/src/main/java/com/developer/admin/query/service/AdminQueryService.java
+++ back-end/with-the-developer/src/main/java/com/developer/admin/query/service/AdminQueryService.java
@@ -4,7 +4,6 @@ import com.developer.admin.query.dto.RecruitApplyDetailReadDTO;
 import com.developer.admin.query.dto.RecruitApplyListReadDTO;
 import com.developer.admin.query.mapper.AdminMapper;
 import lombok.RequiredArgsConstructor;
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,22 +12,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AdminQueryService {
 
-    private final SqlSession sqlSession;
+    private final AdminMapper adminMapper;
 
-//    private final AdminMapper adminMapper;
     // 채용공고 등록 신청 내역 목록 조회
     public List<RecruitApplyListReadDTO> readRecruitApplyList(Integer page) {
         int offset = (page - 1) * 10;
-        return sqlSession.getMapper(AdminMapper.class).readRecruitApplyList(offset);
-//        return adminMapper.readRecruitApplyList(offset);
+
+        return adminMapper.readRecruitApplyList(offset);
     }
 
 
     // 채용공고 등록 신청 상세내역 조회
     public RecruitApplyDetailReadDTO readRecruitApplyDetailById(Long id) {
 
-        return sqlSession.getMapper(AdminMapper.class).readRecruitApplyDetailById(id);
-//        return adminMapper.readRecruitApplyDetailById(id);
+        return adminMapper.readRecruitApplyDetailById(id);
     }
 
 }
