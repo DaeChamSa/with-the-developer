410ef81996b7542107fcd0da24532c37666d76b4
hyomee2 <s5763305@naver.com>
2024-09-29 21:18:56 +0900

    chore: #89 상수화

--- back-end/with-the-developer/src/test/java/com/developer/recruit/command/service/RecruitCommandServiceTest.java
+++ back-end/with-the-developer/src/test/java/com/developer/recruit/command/service/RecruitCommandServiceTest.java
@@ -61,8 +61,12 @@ class RecruitCommandServiceTest {
     @Transactional
     @DisplayName(value = "등록된 채용공고를 삭제할 수 있다.")
     void deleteRecruit() {
+        // Given
+        Long recruitCode = 28L;
+        Long userCode = 3L;
+
         // When
-        recruitCommandService.deleteRecruit(28L, 3L);
+        recruitCommandService.deleteRecruit(recruitCode, userCode);
         Recruit recruit = recruitRepository.findById(28L)
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_POST));
 
@@ -74,11 +78,16 @@ class RecruitCommandServiceTest {
     @Transactional
     @DisplayName(value = "본인이 등록하지 않은 채용공고를 삭제하려고 할 경우 에러가 발생한다.")
     void deleteRecruitByOther() {
+        // Given
+        Long unAuthorizedUserCode = 4L;
+        Long recruitCode = 29L;
+
         // When
         CustomException exception = assertThrows(CustomException.class, () -> {
-            recruitCommandService.deleteRecruit(29L, 4L);
+            recruitCommandService.deleteRecruit(recruitCode, unAuthorizedUserCode);
         });
-        Recruit recruit = recruitRepository.findById(29L)
+
+        Recruit recruit = recruitRepository.findById(recruitCode)
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_POST));
 
         // Then
