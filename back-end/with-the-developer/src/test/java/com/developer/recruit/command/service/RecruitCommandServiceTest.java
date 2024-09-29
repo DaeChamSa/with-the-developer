4224864beba83729d80912992665664a40868f5b
hyomee2 <s5763305@naver.com>
2024-09-29 21:56:47 +0900

    test: #89 채용공고 마감 관련 테스트 코드 작성

--- back-end/with-the-developer/src/test/java/com/developer/recruit/command/service/RecruitCommandServiceTest.java
+++ back-end/with-the-developer/src/test/java/com/developer/recruit/command/service/RecruitCommandServiceTest.java
@@ -35,6 +35,8 @@ class RecruitCommandServiceTest {
     @DisplayName(value = "채용공고 등록을 신청할 수 있다.")
     void applyRecruit() {
         // Given
+        Long userCode = 3L;
+
         RecruitApplyDTO recruitApplyDTO = new RecruitApplyDTO();
         recruitApplyDTO.setRecruitTitle("신입 백엔드 개발자 모집합니다.");
         recruitApplyDTO.setRecruitContent("SpringBoot 경험자 우대");
@@ -44,7 +46,7 @@ class RecruitCommandServiceTest {
         recruitApplyDTO.setJobTagNames(List.of(new String[]{"백엔드"}));
 
         // When
-        recruitCommandService.applyRecruit(recruitApplyDTO, 3L);
+        recruitCommandService.applyRecruit(recruitApplyDTO, userCode);
 
         //Then
         RecruitDetailReadDTO recruitDetailReadDTO = recruitMapper.readRecruitDetailById(recruitRepository.findLatestRecruitCode());
@@ -57,6 +59,61 @@ class RecruitCommandServiceTest {
         recruitRepository.deleteById(recruitRepository.findLatestRecruitCode());
     }
 
+    // 스케줄링 로직 제외 후 테스트
+    @Test
+    @Transactional
+    @DisplayName(value = "")
+    void completeRecruitAuto() {
+        // Given
+        // recruitCode 2인 채용공고의 마감 기한은 지나있다.
+        Long recruitCode = 2L;
+
+        // When
+        recruitCommandService.CompleteRecruitAuto();
+
+        // Then
+        Recruit recruit = recruitRepository.findById(recruitCode)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_POST));
+        assertEquals(RecruitStatus.COMPLETED, recruit.getRecruitStatus());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName(value = "본인이 등록한 채용공고를 수동으로 마감할 수 있다.")
+    void completeRecruitManual() {
+        // Given
+        Long recruitCode = 2L;
+        Long userCode = 3L;
+
+        // When
+        recruitCommandService.completeRecruitManual(recruitCode, userCode);
+
+        // Then
+        Recruit recruit = recruitRepository.findById(recruitCode)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_POST));
+        assertEquals(RecruitStatus.COMPLETED, recruit.getRecruitStatus());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName(value = "본인이 등록하지 않은 채용공고를 마감하려고 할 경우 에러가 발생한다.")
+    void completeRecruitManualByOther() {
+        // Given
+        Long recruitCode = 2L;
+        Long unAuthorizedUserCode = 4L;
+
+        // When
+        CustomException exception = assertThrows(CustomException.class, () -> {
+            recruitCommandService.completeRecruitManual(recruitCode, unAuthorizedUserCode);
+        });
+
+        // Then
+        Recruit recruit = recruitRepository.findById(recruitCode)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_POST));
+        assertEquals(ErrorCode.UNAUTHORIZED_USER, exception.getErrorCode());
+        assertNotEquals(RecruitStatus.COMPLETED, recruit.getRecruitStatus());
+    }
+
     @Test
     @Transactional
     @DisplayName(value = "등록된 채용공고를 삭제할 수 있다.")
@@ -67,10 +124,10 @@ class RecruitCommandServiceTest {
 
         // When
         recruitCommandService.deleteRecruit(recruitCode, userCode);
-        Recruit recruit = recruitRepository.findById(28L)
-                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_POST));
 
         // Then
+        Recruit recruit = recruitRepository.findById(recruitCode)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_POST));
         assertEquals(RecruitStatus.DELETE, recruit.getRecruitStatus());
     }
 
@@ -87,10 +144,9 @@ class RecruitCommandServiceTest {
             recruitCommandService.deleteRecruit(recruitCode, unAuthorizedUserCode);
         });
 
+        // Then
         Recruit recruit = recruitRepository.findById(recruitCode)
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_POST));
-
-        // Then
         assertEquals(ErrorCode.UNAUTHORIZED_USER, exception.getErrorCode());
         assertNotEquals(RecruitStatus.DELETE, recruit.getRecruitStatus());
     }
