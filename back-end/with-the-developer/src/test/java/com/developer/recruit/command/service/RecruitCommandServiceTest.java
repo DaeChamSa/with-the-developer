ee385a83c80b2023380fd700f52bccbf6a1a6093
hyomee2 <s5763305@naver.com>
2024-09-29 17:26:39 +0900

    test: #89 등록된 채용공고 삭제하기 테스트 코드 작성

--- back-end/with-the-developer/src/test/java/com/developer/recruit/command/service/RecruitCommandServiceTest.java
+++ back-end/with-the-developer/src/test/java/com/developer/recruit/command/service/RecruitCommandServiceTest.java
@@ -1,6 +1,10 @@
 package com.developer.recruit.command.service;
 
+import com.developer.common.exception.CustomException;
+import com.developer.common.exception.ErrorCode;
 import com.developer.recruit.command.dto.RecruitApplyDTO;
+import com.developer.recruit.command.entity.Recruit;
+import com.developer.recruit.command.entity.RecruitStatus;
 import com.developer.recruit.command.repository.RecruitRepository;
 import com.developer.recruit.query.dto.RecruitDetailReadDTO;
 import com.developer.recruit.query.mapper.RecruitMapper;
@@ -8,6 +12,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -50,5 +55,20 @@ class RecruitCommandServiceTest {
         assertEquals(LocalDateTime.parse("2024-10-01T09:00:00"), recruitDetailReadDTO.getRecruitEnd());
         assertEquals(List.of(new String[]{"백엔드"}), recruitDetailReadDTO.getJobTagNames());
         recruitRepository.deleteById(recruitRepository.findLatestRecruitCode());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName(value = "등록된 채용공고를 삭제할 수 있다.")
+    void deleteRecruit() {
+        // Given
+
+        // When
+        recruitCommandService.deleteRecruit(28L, 3L);
+        Recruit recruit = recruitRepository.findById(28L)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_POST));
+        // Then
+        assertEquals(RecruitStatus.DELETE, recruit.getRecruitStatus());
+
     }
 }
