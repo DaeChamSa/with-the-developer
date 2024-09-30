31ae3f98d0360b0c7efba32b98540a709a0e8c44
SaeRyung <101532588+SaeRyung@users.noreply.github.com>
2024-09-30 12:02:09 +0900

    [feat/goods/create] 굿즈 crud 기능 구현 (#101)
    * feat: #65 goods create, update 기능 구현
    
    * feat: #65 굿즈 cud 기능 구현
    
    * feat: #65 굿즈 read 기능 구현
    
    * feat: #65 굿즈 read 기능 구현
    
    * chore: comupost 날짜 부분 BaseEntity로 수정
    
    * feat: #65 goods read 기능 구현중
    
    * full 위한 커밋
    
    * chore: 불필요 ComuPost 파일 삭제
    
    * chore: 이미지 굿즈 메서드 추가
    
    * chore: 이미지 굿즈 메서드 추가
    
    * fix : #65 굿즈 날짜 자동 추가 이슈 수정
    
    * chore: #65 굿즈 xml select id 수정 및 이미지 변수 통일 수정 완료

---
+++ back-end/with-the-developer/src/main/java/com/developer/goods/command/application/dto/GoodsUpdateDTO.java
@@ -0,0 +1,15 @@
+package com.developer.goods.command.application.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GoodsUpdateDTO {
+    private long goodsCode;
+    private String goodsName;
+    private String goodsContent;
+    private int goodsStock;
+    private String goodsStatus;
+    private int goodsPrice;
+}
