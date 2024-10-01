82c0cfc541d8285cb490e6c3457eb74a8f9ebcfd
SaeRyung <sr1094@naver.com>
2024-10-02 03:41:51 +0900

    test/comu-cmt: #143 커뮤니티 댓글 테스트 삭제  구현 완료

--- back-end/with-the-developer/src/test/java/com/developer/comu/comment/command/service/ComuCmtServiceTest.java
+++ back-end/with-the-developer/src/test/java/com/developer/comu/comment/command/service/ComuCmtServiceTest.java
@@ -37,6 +37,7 @@ class ComuCmtServiceTest {
     @DisplayName("커뮤니티 게시글 댓글 등록 테스트")
     void createComuCmtTest() {
         // given
+        // 댓글 등록 위한 로그인, 게시글 존재 확인
         // 로그인한 사용자 아이디
         String userId = "user03"; // 유효한 사용자 ID로 교체필수
         Long comuPostCode = 4L; // 유효한 커뮤니티 게시글 코드로 교체필수
@@ -77,6 +78,7 @@ class ComuCmtServiceTest {
         comuPostRepository.findById(comuPostCode)
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_POST));
 
+        // 댓글 등록
         ComuCmtCreateDTO comuCmtCreateDTO = new ComuCmtCreateDTO();
         comuCmtCreateDTO.setComuCmtContent("커뮤니티 댓글 등록");
 
@@ -96,7 +98,35 @@ class ComuCmtServiceTest {
         //then
         assertNotNull(updateCmtId);
         assertEquals("커뮤니티 댓글 수정",updateCmt.getComuCmtContent());
+    }
 
+    @Test
+    @DisplayName("커뮤니티 게시글 댓글 삭제 테스트")
+    void deleteComuCmtTest() {
+        // given
+        // 댓글 수정 위한 로그인, 게시글 확인, 댓글 등록
+        // 로그인한 사용자 아이디
+        String userId = "user03"; // 유효한 사용자 ID로 교체필수
+        Long comuPostCode = 4L; // 유효한 커뮤니티 게시글 코드로 교체필수
 
+        // 사용자 존재 여부 확인
+        User user = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+
+        // 게시글 존재 확인
+        comuPostRepository.findById(comuPostCode)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_POST));
+
+        // 댓글 등록
+        ComuCmtCreateDTO comuCmtCreateDTO = new ComuCmtCreateDTO();
+        comuCmtCreateDTO.setComuCmtContent("커뮤니티 댓글 등록");
+
+        Long createdCmtId = comuCmtService.createComuCmt(comuPostCode, user.getUserCode(), comuCmtCreateDTO);
+
+        //when
+        comuCmtService.deleteComuCmt(createdCmtId, user.getUserCode());
+
+        //then
+        assertTrue(comuCmtRepository.findById(createdCmtId).isEmpty());
     }
 }
