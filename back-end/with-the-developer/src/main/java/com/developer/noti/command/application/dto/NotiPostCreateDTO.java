f6bf5c2bec8749c98e47ba28944293f6baf321fd
PBEM22 <dlacofbs1229@gmail.com>
2024-10-01 23:25:10 +0900

    feat: #131 noti 채용공고 승인 알림, 쪽지 알림 추가

--- back-end/with-the-developer/src/main/java/com/developer/noti/command/application/dto/NotiPostCreateDTO.java
+++ back-end/with-the-developer/src/main/java/com/developer/noti/command/application/dto/NotiPostCreateDTO.java
@@ -4,7 +4,7 @@ import com.developer.noti.command.domain.aggregate.PostType;
 import lombok.Data;
 
 @Data
-public class NotiCreateDTO {
+public class NotiPostCreateDTO {
 
     private Long userCode;
     private Long postCode;
