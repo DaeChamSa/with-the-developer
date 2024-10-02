75ad7d21c8cd26350b278c81ace4996acade00c3
hyomee2 <s5763305@naver.com>
2024-10-02 14:29:20 +0900

    feat: #163 직무태그 조회 기능 구현

---
+++ back-end/with-the-developer/src/test/java/com/developer/report/command/service/ReportCommandServiceTest.java
@@ -0,0 +1,27 @@
+package com.developer.report.command.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.transaction.annotation.Transactional;
+
+class ReportCommandServiceTest {
+
+    @Test
+    void createAndHandleReport() {
+    }
+
+    @Test
+    @Transactional
+    @DisplayName(value = "신고를 생성할 수 있다.")
+    void createReport() {
+        //
+    }
+
+    @Test
+    void handleReport() {
+    }
+
+    @Test
+    void activeUser() {
+    }
+}
