e4402bdc515e1a18ba364e6275cc040137262b80
hyomee2 <s5763305@naver.com>
2024-10-02 11:42:00 +0900

    test: #154 존재하는 신고 사유 카테고리를 등록할 경우 테스트

--- back-end/with-the-developer/src/test/java/com/developer/admin/command/service/AdminCommandServiceTest.java
+++ back-end/with-the-developer/src/test/java/com/developer/admin/command/service/AdminCommandServiceTest.java
@@ -133,6 +133,27 @@ class AdminCommandServiceTest {
 
     @Test
     @Transactional
+    @DisplayName(value = "존재하는 신고 카테고리를 등록하려 할 경우 에러가 발생한다.")
+    void createDuplicatedReportReasonCategory() {
+        // Given
+        String newReportReasonCategory = "부적절한 게시물입니다.";
+
+        // When
+        CustomException exception = assertThrows(CustomException.class, () -> {
+            adminCommandService.createReportReasonCategory(newReportReasonCategory);
+        });
+
+        // Then
+        // 현재 DB에는 "부적절한 게시물입니다."라는 신고 사유 카테고리가 존재하므로 같은 이름의 신고 사유 카테고리를 등록할 시 에러 발생
+        assertEquals(ErrorCode.DUPLICATE_VALUE, exception.getErrorCode());
+
+        // 존재하는 신고 사유 카테고리를 등록하려 하는 경우 에러가 발생하므로 개수는 기존에 존재하던 것과 같아야 한다.
+        List<ReportReasonCategory> reportReasonCategoryList = reportReasonCategoryRepository.findAll();
+        assertEquals(1, reportReasonCategoryList.size());
+    }
+
+    @Test
+    @Transactional
     @DisplayName(value = "NULL인 신고 사유 카테고리를 생성하려 할 경우 에러가 발생하다.")
     void createNullReportReasonCategory() {
         // Given
