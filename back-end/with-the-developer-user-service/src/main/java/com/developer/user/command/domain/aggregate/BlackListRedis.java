fdcdc71ce6349e921f7af5325e5e227938de116f
PBEM22 <dlacofbs1229@gmail.com>
2024-10-08 11:13:51 +0900

    refector: #217 refreshToken 테이블, accessToken blackList 테이블 모두 Redis로 변경

--- back-end/with-the-developer-user-service/src/main/java/com/developer/user/command/domain/aggregate/BlackListRedis.java
+++ back-end/with-the-developer-user-service/src/main/java/com/developer/user/command/domain/aggregate/BlackListRedis.java
@@ -9,22 +9,12 @@ import org.springframework.data.redis.core.index.Indexed;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
-@RedisHash(value = "refresh_token", timeToLive = 1000 * 60 * 60 * 24 * 7)   // 유효 7일
-public class RefreshTokenRedis {
+@RedisHash(value = "black_list", timeToLive = 1000 * 60 * 30)   // 유효 30분
+public class BlackListRedis {
 
     @Id
-    private String userId;
-
-    @Indexed
     private String accessToken;
 
     @Indexed
-    private String refreshToken;
-
-    // 토큰 업데이트
-    public void updateAccessToken(String accessToken, String refreshToken){
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-    }
-
+    private String userId;
 }
