e92b8d13280ecfd8a6a4d7792df0130fba7fb802
PBEM22 <dlacofbs1229@gmail.com>
2024-10-20 19:34:27 +0900

    chore: #230 app.js 수정

--- front-end/with-the-developer/src/App.vue
+++ front-end/with-the-developer/src/App.vue
@@ -8,20 +8,10 @@ import MypageSideBar from "@/components/MypageSideBar.vue";
 <template>
   <div class="page-container">
     <MypageSideBar />
-    <div class="content">
-      <!-- Page-specific content goes here -->
-    </div>
+
   </div>
 </template>
 
 <style scoped>
-.page-container {
-  display: flex;
-}
-
-.content {
-  flex-grow: 1;
-  padding: 20px;
-}
 
 </style>
