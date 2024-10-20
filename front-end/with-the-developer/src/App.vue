630d6a07131abdd441254b743681e6bb7087f457
PBEM22 <dlacofbs1229@gmail.com>
2024-10-20 19:28:06 +0900

    feat: #230 공용 컴포넌트 Mypage 사이드바 생성

--- front-end/with-the-developer/src/App.vue
+++ front-end/with-the-developer/src/App.vue
@@ -1,46 +1,27 @@
 <script setup>
 
+import MypageSideBar from "@/components/MypageSideBar.vue";
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="page-container">
+    <MypageSideBar />
+    <div class="content">
+      <!-- Page-specific content goes here -->
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.page-container {
+  display: flex;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.content {
+  flex-grow: 1;
+  padding: 20px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
