43bbda7c072d84152ea65f387c8e237fede778af
hyomee2 <s5763305@naver.com>
2024-10-21 22:24:57 +0900

    feat: #244 서치바 컴포넌트 디자인 및 라우팅

--- front-end/with-the-developer/src/App.vue
+++ front-end/with-the-developer/src/App.vue
@@ -2,11 +2,14 @@
 
 import Footer from "@/components/Footer.vue";
 import TosView from "@/views/TosView.vue";
+import SearchBar from "@/components/SearchBar.vue";
 </script>
 
 <template>
   <main class="main">
     <TosView/>
+    <SearchBar/>
+    <router-view/> <!--테스트-->
   </main>
 
   <footer>
--- front-end/with-the-developer/src/main.js
+++ front-end/with-the-developer/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router';
 
-createApp(App).mount('#app')
+const app = createApp(App); // Vue 애플리케이션 생성
+app.use(router);  // 애플리케이션에 라우터 추가
+app.mount('#app');
+
