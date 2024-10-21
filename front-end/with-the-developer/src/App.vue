289d2e0cc653194df79ce75c63dca4e1c456f657
PBEM22 <dlacofbs1229@gmail.com>
2024-10-21 14:27:37 +0900

    feat: #240 약관동의 페이지

--- front-end/with-the-developer/src/App.vue
+++ front-end/with-the-developer/src/App.vue
@@ -1,12 +1,18 @@
 <script setup>
 
+import TosView from "@/views/TosView.vue";
 </script>
 
 <template>
-  <main>
+  <main class="main">
+    <TosView/>
   </main>
 </template>
 
 <style scoped>
+  *{
+    font-family: "Neo둥근모 Pro";
+    margin: 0 auto;
+  }
 
 </style>
