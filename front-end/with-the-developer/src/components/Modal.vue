ca6334720277d04c4c058edf33094cd036d0002a
SaeRyung <sr1094@naver.com>
2024-10-23 15:28:55 +0900

    feat/chore 모달창 구현완료,  굿즈 등록 버튼 시 등록 창, 닫기 버튼 구현 완료

---
+++ front-end/with-the-developer/src/components/Modal.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="modal-overlay" v-if="show">
+    <div class="modal-content">
+      <button class="close-button" @click="close">X</button>
+      <slot></slot>
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+import {defineProps, defineEmits} from 'vue';
+
+const props = defineProps({
+  show:{
+    type: Boolean,
+    default: false
+  }
+});
+
+const emit = defineEmits(['close']);
+
+const close = () => {
+  emit('close');
+}
+
+</script>
+
+<style scoped>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+
+.modal-content {
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
+  width: 50vw;
+  height: 60vh;
+  position: relative;
+}
+
+.close-button {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: none;
+  border: none;
+  font-size: 18px;
+  cursor: pointer;
+}
+
+.close-button:hover {
+  color: red;
+}
+</style>
